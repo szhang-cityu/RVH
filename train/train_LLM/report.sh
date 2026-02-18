f68ba5ff5cfc33fb3f7e9887ae3bd58747358907
Sheng Zhang <152848201+szhang-cityu@users.noreply.github.com>
2026-02-18 17:41:30 +0800

    update

--- train/train_LLM/report.sh
+++ train/train_LLM/report.sh
@@ -1,5 +1,5 @@
 python /home/szhang844/SFT/report_generate.py \
-  --test_json /home/szhang844/SFT/abnormal_test.json \
-  --output_json /home/szhang844/SFT/results/report_results.json \
-  --base_model /home/szhang844/SFT/merge \
-  --grpo_lora /home/szhang844/SFT/grpo_lora
+  --test_json /your_source_data \
+  --output_json /home/.../report_results.json \
+  --base_model /home/.../merge \
+  --grpo_lora /home/.../grpo_lora
